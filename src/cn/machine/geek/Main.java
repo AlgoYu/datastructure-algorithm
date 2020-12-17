717422f0c07038abd6ded6d1e3f493bcca88198b
MachineGeek <794763733@qq.com>
2020-12-17 15:34:36 +0800

    add algorithm for josephus problem.

--- src/cn/machine/geek/Main.java
+++ src/cn/machine/geek/Main.java
@@ -1,6 +1,6 @@
 package cn.machine.geek;
 
-import cn.machine.geek.structure.CircleTwoWayLinkedList;
+import cn.machine.geek.algorithm.Josephus;
 
 
 /**
@@ -11,14 +11,11 @@ import cn.machine.geek.structure.CircleTwoWayLinkedList;
  */
 public class Main {
     public static void main(String[] args) {
-        CircleTwoWayLinkedList<Integer> linkedList = new CircleTwoWayLinkedList<>();
-        linkedList.insert(0,0);
-        linkedList.insert(1,1);
-        linkedList.insert(0,-1);
-        linkedList.add(2);
-        linkedList.remove(3);
-        linkedList.remove(0);
-        linkedList.remove(0);
-        linkedList.remove(0);
+        Josephus<Integer> josephus = new Josephus<>();
+        josephus.add(1);
+        josephus.add(2);
+        josephus.add(3);
+        josephus.add(4);
+        josephus.josephusProblem(4,3);
     }
 }
