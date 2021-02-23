806163d2e5aa623fa9ef13885cfe684de5ba3d95
MachineGeek <794763733@qq.com>
2021-02-23 15:39:20 +0800

    add binary search.

--- src/cn/machine/geek/Main.java
+++ src/cn/machine/geek/Main.java
@@ -1,5 +1,6 @@
 package cn.machine.geek;
 
+import cn.machine.geek.algorithm.search.BinarySearch;
 import cn.machine.geek.algorithm.sort.BubbleSort;
 import cn.machine.geek.algorithm.sort.HeapSort;
 import cn.machine.geek.algorithm.sort.InsertionSort;
@@ -20,14 +21,17 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
         Integer[] elements = new Integer[]{3,4,4,5,6,1,10,2,9,7,8};
-        new InsertionSort<Integer>().sort(elements, new Comparator<Integer>() {
+        Comparator<Integer> comparator = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o1 - o2;
             }
-        });
+        };
+        new InsertionSort<Integer>().sort(elements, comparator);
         long time = System.currentTimeMillis();
         System.out.println(Arrays.toString(elements));
         System.out.println(System.currentTimeMillis() - time);
+
+        System.out.println(new BinarySearch<Integer>().search(elements,2,comparator));
     }
 }
---
+++ src/cn/machine/geek/algorithm/search/BinarySearch.java
@@ -0,0 +1,38 @@
+package cn.machine.geek.algorithm.search;
+
+import java.util.Comparator;
+
+/**
+ * @Author: MachineGeek
+ * @Description: 二分搜索
+ * @Email: 794763733@qq.com
+ * @Date: 2021/2/23
+ */
+public class BinarySearch<E> {
+    public int search(E[] elements,E element, Comparator<E> comparator){
+        if(elements == null || comparator == null || elements.length < 2){
+            return -1;
+        }
+        // 初始化左右边界
+        int left = 0,right = elements.length - 1;
+        // 循环查找排除另一半
+        while (left <= right){
+            // 获得中间索引下标
+            int mid = (left + right) >> 1;
+            // 比较
+            int value = comparator.compare(element,elements[mid]);
+            // 如果要查找的元素比中间元素大，排除左边一半。
+            if(value > 0){
+                left = mid + 1;
+            // 如果要查找的元素比中间元素小，排除左边一半。
+            }else if(value < 0){
+                right = mid - 1;
+            // 元素相等，返回中间索引下标
+            }else{
+                return mid;
+            }
+        }
+        // 找不到就返回负数
+        return -1;
+    }
+}
